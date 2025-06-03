aa7bbab0254f1d25d6bcd6c35891b4759732ce49
nhnhan18 <104907284+nhnhan18@users.noreply.github.com>
2025-06-04 00:48:10 +0700

    Update sku.py

--- sku.py
+++ sku.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import undetected_chromedriver as uc
 
-options = uc.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-driver = uc.Chrome(options=options)
+    options = uc.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    driver = uc.Chrome(options=options)
 
     try:
         driver.get(url)
